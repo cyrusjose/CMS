d0dcf6529f74a3b31e3fbbfbe50e54c86cf8a8f5
Cyrus Jose <cyrus.jose@outlook.com>
2020-07-31 12:28:33 -0700

    Added a switch statment to handle choices

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var inquirer = require('inquirer');
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user'root',
+    user:'root',
     password: 'Pse2udograph2#$',
     database: 'cms_db'
 });
@@ -15,16 +15,51 @@ connection.connect(function(err){
     start();
 });
 
+// There are a total of 7 items in the array currently. Since index starts at 0 the frist item is 0 and the last item is 6.
+const questions = ['Add a department', 'Add an employee', 'Add a role', 'View and employee', 'View a department', 'View a role', 'Update employee role']
+
 function start() {
-    // There are a total of 7 items in the array currently. Since index starts at 0 the frist item is 0 and the last item is 6.
-    const questions = ['Add a department', 'Add an employee', 'Add a role', 'View and employee', 'View a department', 'View a role', 'Update employee role']
     inquirer
     .prompt({
-        name: 'prelimQuestion'
+        name: 'prelimQuestion',
         type: 'list',
-        message: 'What would you like to do?'
+        message: 'What would you like to do?',
         choices: questions
-    }).then(answer =>{
-
-    }
+    })
+    .then(function(answer){
+        switch(answer.prelimQuestion){
+            case answer.choices === questions[0]:
+                addDepartment();
+                start();
+                break;
+            case answer.choices === questions[1]:
+                addEmployee();
+                start();
+                break;
+            case answer.choices === questions[2]:
+                addRole();
+                start();
+                break;
+            case answer.choices === questions[3]:
+                viewEmmployee();
+                start();
+                break;
+            case answer.choices === questions[4]:
+                viewDepartment();
+                start();
+                break;
+            case answer.choices === questions[5]:
+                viewRole();
+                start();
+                break;
+            case answer.choices === questions[6]:
+                upadateEmployeeRole();
+                start();
+                break;
+            default:
+                'Please choose and option';
+                start();
+                break
+        };
+    });
 }
