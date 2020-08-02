cb7e420f64fc1aa9d26ba89245f0bcdb9345928a
Cyrus Jose <cyrus.jose@outlook.com>
2020-08-01 19:10:50 -0700

    Fixed schema

--- db/schema.sql
+++ db/schema.sql
@@ -22,9 +22,7 @@ CREATE TABLE staffRole (
     department_id INT NOT NULL,
     -- Take a colum of a table and cache it so that we can refernce it frequently.
     --  ON DELETE CASCADE will delete the reference to the other table once the code is done running. 
-    INDEX dep_id (department_id), 
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES 
-    department (id) ON DELETE CASCADE, 
+    FOREIGN KEY (department_id) REFERENCES department (id), 
     PRIMARY KEY(id)
 );
 
@@ -34,10 +32,9 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    INDEX ro_id (role_id), 
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES 
-    staffRole (id) ON DELETE CASCADE,
-    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES 
+    staffRole (id),
+    FOREIGN KEY (manager_id) REFERENCES  employee (id),
     PRIMARY KEY(id)
 );
 
--- server.js
+++ server.js
@@ -1,130 +1,114 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-var connection = require('./config/connection')
-
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user:'root',
-    password: 'Pse2udograph2#$',
-    database: 'cms_db'
-});
-
-connection.connect(function(err){
-    if(err) throw err;
-    
-    start();
-});
+var inquirer = require("inquirer");
+var connection = require("./config/connection");
 
 // There are a total of 7 items in the array currently. Since index starts at 0 the frist item is 0 and the last item is 6.
-const questions = ['Add a department', 'Add an employee', 'Add a role', 'View and employee', 'View a department', 'View a role', 'Update employee role']
+const questions = [
+  "Add a department",
+  "Add an employee",
+  "Add a role",
+  "View and employee",
+  "View a department",
+  "View a role",
+  "Update employee role",
+];
 
 function start() {
-    inquirer
+  inquirer
     .prompt({
-        name: 'prelimQuestion',
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: questions
+      name: "prelimQuestion",
+      type: "list",
+      message: "What would you like to do?",
+      choices: questions,
     })
-    .then(function(answer){
-        switch(answer.prelimQuestion){
-            // Case does not evaluate anything.
-            case questions[0]:
-                addDepartment();
-                break;
-            case questions[1]:
-                addEmployee();
-                break;
-            case questions[2]:
-                addRole();
-                break;
-            case questions[3]:
-                viewEmployee();
-                break;
-            case questions[4]:
-                viewDepartment();
-                break;
-            case questions[5]:
-                viewRole();
-                break;
-            case questions[6]:
-                upadateEmployeeRole();
-                break;
-            default:
-                'Please choose and option';
-                break;
-        };
+    .then(function (answer) {
+      switch (answer.prelimQuestion) {
+        // Case does not evaluate anything.
+        case questions[0]:
+          addDepartment();
+          break;
+        case questions[1]:
+          addEmployee();
+          break;
+        case questions[2]:
+          addRole();
+          break;
+        case questions[3]:
+          viewEmployee();
+          break;
+        case questions[4]:
+          viewDepartment();
+          break;
+        case questions[5]:
+          viewRole();
+          break;
+        case questions[6]:
+          upadateEmployeeRole();
+          break;
+        default:
+          "Please choose and option";
+          break;
+      }
     });
 }
-let depArr = [];
-let addDepartment = ()=>{
-    inquirer
-    .prompt ([
+
+let addDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        name: "addDepartment",
+        type: "input",
+        message: "What's the name of the department",
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        "INSERT INTO department SET ?",
         {
-            name: 'addDepartment',
-            type: 'input',
-            message: 'What\'s the name of the department'
+          name: answer.addDepartment,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log("You've successfully added a department");
+          start();
         }
-    ]).then(answer => {
-        connection.query(
-            'INSERT INTO department SET ?', 
-            {
-                name: answer.addDepartment
-            },
-            (err)=>{
-                if (err) throw err;
-                console.log('You\'ve successfully added a department')
-                start();
-            },
-            depArr.push(answer.addDepartment)
-        )
+      );
     });
 };
 
 let addRole = () => {
-    
+    connect
 };
 
-let addEmployee = () =>{
-    inquirer
+let addEmployee = () => {
+  inquirer
     .prompt([
-        {
-            name: 'empTitle',
-            type: 'input',
-            message: 'What is the title of the employee'
-        },
-        {
-            name: 'empSalary',
-            type: 'input',
-            message: 'What is the salary'
-        },
-        // {Add to staff role}
-            name: 'departments',
-            type: 'list',
-            message: 'Choose a department',
-            choices: depArr
-        }
-    ]).then(answer => {
-        connection.query(
-            'SELECT * FROM cms_db',
-            {
-
-            }
-        )
-    })
+      {
+        name: "empTitle",
+        type: "input",
+        message: "What is the title of the employee",
+      },
+      {
+        name: "empSalary",
+        type: "input",
+        message: "What is the salary",
+      },
+      // {Add to staff role}
+      {
+        name: "departments",
+        type: "list",
+        message: "Choose a department",
+        choices: depArr,
+      },
+    ])
+    .then((answer) => {
+      connection.query("SELECT * FROM cms_db", {});
+    });
 };
 
-let viewDepartment = () =>{
+let viewDepartment = () => {};
+let viewEmployee = () => {};
+let viewRole = () => {};
 
-};
-let viewEmployee = () =>{
+let upadateEmployeeRole = () => {};
 
-}
-let viewRole = () => {
-
-};
-
-let upadateEmployeeRole = () =>{
-
-};
+start();
